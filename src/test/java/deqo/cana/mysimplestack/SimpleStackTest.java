4b1c4187f49534a701f2c173a24b185cb03150a3
Cyril A <cyril.anaclet@gmail.com>
2020-10-26 14:28:26 +0100

    fix: Coverage 100%

--- src/test/java/deqo/cana/mysimplestack/SimpleStackTest.java
+++ src/test/java/deqo/cana/mysimplestack/SimpleStackTest.java
@@ -1,7 +1,6 @@
 package deqo.cana.mysimplestack;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.EmptyStackException;
@@ -22,10 +21,10 @@ public class SimpleStackTest {
     }
 
     @Test
-    public void testCreateEmptyStack() {
-
+    public void StackConstructor(){
+        SimpleStack stack2 = new SimpleStack(item1,item2);
+        assertEquals("The new stack should contains 2 items",2,stack2.getSize());
     }
-
     @Test
     public void isEmpty() {
         assertTrue("A new stack should be empty", stack.isEmpty());
@@ -42,7 +41,7 @@ public class SimpleStackTest {
         assertEquals("A new stack has no element", 0, stack.getSize());
     }
 
-    @Test
+    @Test //Push is also tested here
     public void getSizeWithTwoElmts() {
         stack.push(item1);
         stack.push(item2);
@@ -50,23 +49,36 @@ public class SimpleStackTest {
     }
 
     @Test
-    public void push() throws EmptyStackException {
-
+    public void pushOutOfBounds() {
+        for (int i = 0; i< 11; i++){
+            stack.push(item1);
+        }
+        assertEquals("Stack shoud have 10 elements", 10, stack.getSize());
     }
 
     @Test
-    public void peek() {
+    public void peek() throws EmptyStackException {
+        stack.push(item1);
+        stack.push(item2);
+        assertSame("Peek should return the last item pushed", item2, stack.peek());
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void peekOnEmptyStack() throws EmptyStackException {
+        stack.peek(); // should throws an EmptyStackException .
     }
 
     @Test
     public void pop() {
+        stack.push(item1);
+        stack.push(item2);
+        assertSame("Pop should return the element popped", item2, stack.pop());
+        stack.pop();
+        assertTrue("two pop should empty the stack", stack.isEmpty());
     }
 
     @Test(expected = EmptyStackException.class)
     public void testPopOnEmptyStack() throws EmptyStackException {
         stack.pop(); // should throws an EmptyStackException .
     }
-
-
-
 }
